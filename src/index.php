40d673236feaccc0c5e1f1b4618182737ca8278c
server <server@example.com>
2018-02-09 09:46:29 -0700

    adjust size

--- src/index.php
+++ src/index.php
@@ -6,17 +6,20 @@ header("Content-Type: image/png");
 header('Content-disposition: inline; filename="image.png"');
 
 /**
- * Do not serve too large image.
+ * Do not serve too large image in order to save server resources.
+ * Adjust new width and height in that case.
  *
  * Example of an image source:
  *   http://pixel.example.com/?w=200&h=100
  */
 
 $width = (int)($_GET["w"]??1);
-$width = ($width < 1 || $width > 1000)?1:$width;
+$width = ($width < 1)?1:$width;
+$width = ($width > 1000)?1000:$width;
 
 $height = (int)($_GET["h"]??1);
-$height = ($height < 1 || $height > 1000)?1:$height;
+$height = ($height < 1)?1:$height;
+$height = ($height > 1000)?1000:$height;
 
 /**
  * When GD not installed or other errors
@@ -24,6 +27,7 @@ $height = ($height < 1 || $height > 1000)?1:$height;
 function error()
 {
     readfile("1x1.png");
+    return null;
 }
 
 if(function_exists("imagecreate"))
