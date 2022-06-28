9e774c9d1067325c53b04eafcb643a49f2bd57fe
Bimal Poudel <anytizer@users.noreply.github.com>
2022-06-28 02:28:26 -0600

    icons added

--- src/index.php
+++ src/index.php
@@ -6,19 +6,33 @@
     <style>
         body { background-color: #FFFFFF; }
         img { border: 1px solid #AAAACC; }
-		input[type="text"] { width: 50px; }
+		input[type="number"] { width: 60px; border: 1px solid purple; }
 		h2 { color: purple; }
-		a { text-decoration: none; border-bottom: 1px solid #FFAA99; }
+		a { text-decoration: none; color: #f59b42; }
     </style>
 </head>
 <body class="w3-white">
 	<div class="w3-container w3-blue">
-		<h1>Image Placeholder</h1>
+		<h1>Placeholder Images</h1>
 	</div>
 	<div class="w3-padding">
 		<p>Serves a blank image having size between 1x1 pixels and 1000x1000 pixels.</p>
 		<p>Hosted on <a href="https://heroku.com/">Heroku</a> &raquo; <a href="https://transparent-images.herokuapp.com/">Transparent Images</a></p>
-		<p>Source code on github: <a href="https://github.com/anytizer/pixel.php">@anytizer/pixel.php</a></p>
+		<p>
+			Source code on github: <a href="https://github.com/anytizer/pixel.php">@anytizer/pixel.php</a>,
+			<a href="https://github.com/anytizer/pixel.php/blob/master/readme.md">FAQ</a>
+		</p>
+
+		<h2>Your own image size</h2>
+		<form method="GET" action="image/" name="chooser">
+			Width (w): <input type="number" name="w" value="20" min="1" max="1000" placeholder="1" required="required" /> x
+			Height (h): <input type="number" name="h" value="20" min="1" max="1000" placeholder="1000" required="required" />
+			<a href="">
+				<img src="image/magnifier-glass.svg" height="20" style="border: none;" />
+			</a>
+			<input type="submit" value="View Image" class="w3-button w3-blue w3-hover-blue" /> |
+			<a href="#" onclick="return _randomize();" class="w3-button w3-purple w3-hover-blue"><img src="image/infinity-loop.svg" height="20" style="border: none;" /> Randomize</a>
+		</form>
 
 		<h2>Examples</h2>
 		<p>
@@ -61,14 +75,12 @@
 			<img src="image/?w=40&amp;h=10" width="90" height="20" />
 		</p>
 		
-		<h2>Your own image size</h2>
-		<form method="GET" action="image/" name="chooser">
-			Width (w): <input type="number" name="w" value="20" min="1" max="1000" placeholder="1" required="required" /> x
-			Height (h): <input type="number" name="h" value="20" min="1" max="1000" placeholder="1000" required="required" />
-			<input type="submit" value="View Image" />
-			<a href="#" onclick="return _randomize();">Random Size</a>
-		</form>
+		<p>
+			<a href="https://uxwing.com/photos-icon/" style="border: none;"><img src="image/photos.svg" style="height: 30px; color: red; border: none;" /></a>
+			Made by: <a href="https://github.com/anytizer">Bimal Poudel</a>
+		</p>
 	</div>
+	
 	<script>
 		function _rand(min, max)
 		{
